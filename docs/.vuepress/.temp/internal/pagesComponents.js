c69d736a6c4ee59bcaa1e303484c73cee55e3b38
linyuanyuan <herryleehom@126.com>
2023-01-13 19:04:26 +0800

    feat: updated vuepress logic

---
+++ docs/.vuepress/.temp/internal/pagesComponents.js
@@ -0,0 +1,8 @@
+import { defineAsyncComponent } from 'vue'
+
+export const pagesComponents = {
+  // path: /
+  "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"/Users/linyuanyuan/Desktop/bluepha/bluepha-vue-low-ui/docs/.vuepress/.temp/pages/index.html.vue")),
+  // path: /404.html
+  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"/Users/linyuanyuan/Desktop/bluepha/bluepha-vue-low-ui/docs/.vuepress/.temp/pages/404.html.vue")),
+}
